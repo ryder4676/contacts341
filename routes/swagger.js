8d8483ab17756803d2db403fce3ac8c22f631edb
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-11-09 09:26:23 -0700

    added comments to swagger files

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,8 +1,18 @@
+
+// This code sets up a route for serving Swagger UI, a tool for visualizing and interacting with an API's documentation. 
+// The Swagger documentation is provided from a JSON file (swagger-output.json). The route is configured to be 
+// accessible at '/api-docs', allowing developers to easily explore and understand the API specifications.
+
+// Importing necessary dependencies for API documentation
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
+// Setting up route for serving Swagger UI
 router.use('/api-docs', swaggerUi.serve);
+
+// Setting up route for displaying Swagger documentation
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
+// Exporting the router for use in the main application
 module.exports = router;
--- swagger.js
+++ swagger.js
@@ -1,5 +1,10 @@
+// This script automates the generation of Swagger documentation for an API, 
+// making it easier for developers to understand and interact with the API endpoints.
+
+// Importing the 'swagger-autogen' package
 const swaggerAutogen = require('swagger-autogen')();
 
+// Defining the basic structure of the Swagger documentation
 const doc = {
   info: {
     title: 'My API',
@@ -8,10 +13,14 @@ const doc = {
   host: 'https://render4676.onrender.com'
 };
 
+// Specifying the output file for the generated Swagger documentation
 const outputFile = './swagger-output.json';
+
+// Defining the routes to be documented (in this case, only './routes/index.js')
 const routes = ['./routes/index.js'];
 
 /* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
 root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
+// Generating Swagger documentation based on the provided configuration
 swaggerAutogen(outputFile, routes, doc);
